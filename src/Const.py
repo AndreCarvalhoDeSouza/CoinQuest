b5bc833ca2bea3a95c2b297aa4a3f96e605b610a
andrecarvalhosouza <andrecdesouza15@gmail.com>
2026-06-18 09:31:26 -0300

    Creating asset folder Inserting all assets Creating static menu window for a while

---
+++ src/Const.py
@@ -0,0 +1,18 @@
+#C
+COLOR_MENU_TITLE = (162, 169, 29)
+COLOR_MENU_OPTIONS = (0, 0, 0)
+
+#I
+INSTRUCTION = {
+
+}
+
+#M
+MENU_OPTION = ("Game - With Player",
+               "Instruction",
+               "Score",
+                "Exit")
+
+#W
+WIN_WIDTH = 960
+WIN_HEIGHT = 540
